8479092ed8012b497386485fc6fd260b48ab5bc3
Carlos Cruz Dominguez <carloscruzdominguez97@gmail.com>
2017-10-03 17:53:40 +0200

    Subo terminado

--- app/src/main/java/com/example/contadorcafes/MainActivity.java
+++ app/src/main/java/com/example/contadorcafes/MainActivity.java
@@ -1,20 +1,20 @@
 package com.example.contadorcafes;
 
-import android.icu.text.SimpleDateFormat;
-import android.icu.util.TimeUnit;
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnMas;
     private Button btnMenos;
     private Button btnEmpezar;
+    private Button btnReestablecer;
     private TextView txtCafes;
     private TextView txtNumero;
     private TextView txtTiempo;
@@ -22,28 +22,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private int tiempo;
     private double minutos;
     private double segundos;
+    private int cafes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnMas = (Button)findViewById(R.id.btnMas);
-        btnMenos = (Button)findViewById(R.id.btnMenos);
-        btnEmpezar = (Button)findViewById(R.id.btnEmpezar);
-        txtCafes = (TextView)findViewById(R.id.txtCafes);
-        txtNumero = (TextView)findViewById(R.id.txtNumero);
-        txtTiempo = (TextView)findViewById(R.id.txtTiempo);
+        btnMas = (Button) findViewById(R.id.btnMas);
+        btnMenos = (Button) findViewById(R.id.btnMenos);
+        btnEmpezar = (Button) findViewById(R.id.btnEmpezar);
+        btnReestablecer = (Button) findViewById(R.id.btnReestablecer);
+        txtCafes = (TextView) findViewById(R.id.txtCafes);
+        txtNumero = (TextView) findViewById(R.id.txtNumero);
+        txtTiempo = (TextView) findViewById(R.id.txtTiempo);
+        cafes = 8;
+        tiempo = 5;
 
+        btnEmpezar.setOnClickListener(this);
+        btnMas.setOnClickListener(this);
+        btnMenos.setOnClickListener(this);
+        btnReestablecer.setOnClickListener(this);
+
+        btnReestablecer.setEnabled(false);
+
+        txtNumero.setText(Integer.toString(cafes));
+        txtTiempo.setText(Integer.toString(tiempo) + ":" + "00");
     }
 
     @Override
     public void onClick(View v) {
         if (v == btnMas) {
-
+            tiempo += 1;
+            txtTiempo.setText(Integer.toString(tiempo) + ":" + "00");
         }
         if (v == btnMenos) {
-
+            if (tiempo > 1)
+                tiempo -= 1;
+            txtTiempo.setText(Integer.toString(tiempo) + ":" + "00");
+        }
+        if (v == btnReestablecer) {
+            btnMenos.setEnabled(true);
+            btnMas.setEnabled(true);
+            btnEmpezar.setEnabled(true);
+            btnReestablecer.setEnabled(false);
+            cafes = 0;
+            txtNumero.setText(Integer.toString(cafes));
         }
         if (v == btnEmpezar) {
             cuenta = new CountDownTimer(tiempo * 60 * 1000, 1000) {
@@ -51,18 +75,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 public void onTick(long millisUntilFinished) {
                     minutos = (millisUntilFinished / 1000) / 60;
                     segundos = (millisUntilFinished / 1000) % 60;
-                    txtTiempo.setText(Integer.toString(Math.round((float)minutos)) + ":" + Integer.toString(Math.round((float)segundos)));
+                    txtTiempo.setText(Integer.toString(Math.round((float) minutos)) + ":" + Integer.toString(Math.round((float) segundos)));
                 }
 
                 @Override
                 public void onFinish() {
+                    MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.pop);
+                    mp.start();
+                    btnMenos.setEnabled(true);
+                    btnMas.setEnabled(true);
+                    btnEmpezar.setEnabled(true);
+                    txtTiempo.setText(Integer.toString(tiempo) + ":" + "00");
+                    if (cafes < 9) {
+                        cafes += 1;
+                        txtNumero.setText(Integer.toString(cafes));
 
+
+                    } else {
+                        cafes += 1;
+                        txtNumero.setText(Integer.toString(cafes));
+                        if (cafes == 10) {
+                            AlertDialog.Builder popup = new AlertDialog.Builder(MainActivity.this);
+                            popup.setTitle("Fin");
+                            popup.setMessage("Fin!");
+                            popup.setPositiveButton("Ok", null);
+                            popup.show();
+                        }
+                        mp.start();
+                        btnMenos.setEnabled(false);
+                        btnMas.setEnabled(false);
+                        btnEmpezar.setEnabled(false);
+                        btnReestablecer.setEnabled(true);
+                    }
                 }
             };
 
-            cuenta.start();
             btnMenos.setEnabled(false);
             btnMas.setEnabled(false);
+            btnEmpezar.setEnabled(false);
+            cuenta.start();
+
         }
     }
 }
