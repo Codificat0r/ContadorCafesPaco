6aed7bd32e5297dff3c0356ae735b99b07c85801
Carlos Cruz Domínguez <carloscruzdominguez97@gmail.com>
2017-10-03 12:50:44 +0200

    Subo

--- app/src/main/java/com/example/contadorcafes/MainActivity.java
+++ app/src/main/java/com/example/contadorcafes/MainActivity.java
@@ -19,8 +19,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView txtNumero;
     private TextView txtTiempo;
     private CountDownTimer cuenta;
-    private TimeUnit tiempo;
-    private int milisegundos = 300000;
+    private int tiempo;
+    private double minutos;
+    private double segundos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,71 +35,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         txtNumero = (TextView)findViewById(R.id.txtNumero);
         txtTiempo = (TextView)findViewById(R.id.txtTiempo);
 
-        cuenta = new CountDownTimer(milisegundos, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                milisegundos -= 1000;
-                txtTiempo.setText(String.format(SimpleDateFormat.MINUTE_SECOND, milisegundos));
-            }
-
-            @Override
-            public void onFinish() {
-                txtTiempo.setText("Fin de la cuenta");
-                txtCafes.setText(Integer.toString(Integer.parseInt(txtCafes.getText().toString()) + 1));
-                btnMenos.setEnabled(true);
-                btnMas.setEnabled(true);
-                milisegundos = 300000;
-            }
-        };
-
-        txtTiempo.setText(String.format(SimpleDateFormat.MINUTE_SECOND, milisegundos));
     }
 
     @Override
     public void onClick(View v) {
         if (v == btnMas) {
-            milisegundos += 60000;
-            cuenta = new CountDownTimer(milisegundos, 1000) {
+
+        }
+        if (v == btnMenos) {
+
+        }
+        if (v == btnEmpezar) {
+            cuenta = new CountDownTimer(tiempo * 60 * 1000, 1000) {
                 @Override
                 public void onTick(long millisUntilFinished) {
-                    milisegundos -= 1000;
-                    txtTiempo.setText(String.format(SimpleDateFormat.MINUTE_SECOND, milisegundos));
+                    minutos = (millisUntilFinished / 1000) / 60;
+                    segundos = (millisUntilFinished / 1000) % 60;
+                    txtTiempo.setText(Integer.toString(Math.round((float)minutos)) + ":" + Integer.toString(Math.round((float)segundos)));
                 }
 
                 @Override
                 public void onFinish() {
-                    txtTiempo.setText("Fin de la cuenta");
-                    txtCafes.setText(Integer.toString(Integer.parseInt(txtCafes.getText().toString()) + 1));
-                    btnMenos.setEnabled(true);
-                    btnMas.setEnabled(true);
-                    milisegundos = 300000;
+
                 }
             };
-            txtTiempo.setText(String.format(SimpleDateFormat.MINUTE_SECOND, cuenta.toString()));
-        }
-        if (v == btnMenos) {
-            if (milisegundos - 60000 >= 0) {
-                milisegundos -= 60000;
-                cuenta = new CountDownTimer(milisegundos, 1000) {
-                    @Override
-                    public void onTick(long millisUntilFinished) {
-                        milisegundos -= 1000;
-                        txtTiempo.setText(String.format(SimpleDateFormat.MINUTE_SECOND, milisegundos));
-                    }
 
-                    @Override
-                    public void onFinish() {
-                        txtTiempo.setText("Fin de la cuenta");
-                        txtCafes.setText(Integer.toString(Integer.parseInt(txtCafes.getText().toString()) + 1));
-                        btnMenos.setEnabled(true);
-                        btnMas.setEnabled(true);
-                        milisegundos = 300000;
-                    }
-                };
-                txtTiempo.setText(String.format(SimpleDateFormat.MINUTE_SECOND, cuenta.toString()));
-            }
-        }
-        if (v == btnEmpezar) {
             cuenta.start();
             btnMenos.setEnabled(false);
             btnMas.setEnabled(false);
